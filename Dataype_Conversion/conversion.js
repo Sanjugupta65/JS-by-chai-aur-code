a7b7abab99cc252c309a6004b85884cf95d2ef98
Sanju <167853871+Sanjugupta65@users.noreply.github.com>
2026-03-09 00:35:59 +0530

    changing the value form one data type to another

---
+++ Dataype_Conversion/conversion.js
@@ -0,0 +1,42 @@
+
+// here we convert string into number
+// let score = "33ab";
+
+// console.log(score);
+// console.log("type of score is :", typeof score)
+
+
+// let valueInNumber = Number(score);    // convert String in Number type
+// console.log("After conversion type of score will be :" , typeof valueInNumber);
+// console.log(valueInNumber );
+// console.log();
+
+
+
+
+
+// here we convert number into string type
+// console.log("now we perform with integer or number value");
+// let age = 33;
+
+// console.log(age);
+// console.log("type of age is :", typeof age)
+
+
+// let valueInString = String(age);    // convert Number  in String  type
+// console.log("After conversion type of age will be :" , typeof valueInString);
+// console.log(valueInString);
+
+
+
+
+
+let isfollow = true;
+
+console.log(isfollow);
+// console.log("type of isfollow : ", typeof isfollow);  
+console.log(`Type is isfollow is ${typeof isfollow}`);  // this is modern to write the above 
+
+let convertisfollow = Number(isfollow);
+console.log("type of isfollow after conversion : ", typeof convertisfollow);
+console.log(convertisfollow);
---
+++ Dataype_Conversion/convertion_operation.js
@@ -0,0 +1,11 @@
+console.log(3+3);   //here it will add values = 3 (treat like interger value)
+
+console.log("3" + "3");   // here it will concatenate the values = 33 (treat like string )
+
+console.log(1 + 3 +"3");  // here first two values are  (1)(3) is integer so add this = 4 and then ("3") is treat like string  so , answer is 43 
+
+console.log("1" + 3 + 3);   //here first value is ("1") , so treat each value like string and concate them , so answer is 133
+
+console.log(3 +  "4" + 4);   // 3 is interger value but after "4" it treat other value like string so concate all , answer is 344
+
+console.log(3 + 4 + "4");    // 3 and 4 is integer which is =  7 and "4" is string , so answer is 74
